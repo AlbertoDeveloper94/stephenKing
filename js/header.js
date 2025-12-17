5371450abaf988ce14a2cf008a785618d15411e5
Alberto <bettogp94@gmail.com>
2025-12-17 13:48:58 +0100

    fix: I inject header js to html tag dinamically

---
+++ js/header.js
@@ -0,0 +1,46 @@
+class MyHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+
+    <header>
+      <nav class="navbar navbar-expand-lg fixed-top borderBottom p-4">
+        <div class="container-fluid">
+          <div class="d-flex align-items-center justify-content-center col-6">
+            <a href="" class="navbar-brand">
+              <img src="../images/logo/StephenKingLogo.png" alt="" class="logoSize" />
+            </a>
+            <h1 class="letterSpacing ms-4">STEPHEN KING</h1>
+          </div>
+          <button
+            class="navbar-toggler"
+            data-bs-toggle="collapse"
+            data-bs-target="#menu"
+          >
+            <span class="navbar-toggler-icon navbar-dark"></span>
+          </button>
+          <div
+            class="navbar-collapse collapse justify-content-around col-6"
+            id="menu"
+          >
+            <ul class="navbar-nav d-flex justify-content-around w-100">
+              <li class="nav-item text-center">
+                <a href="" class="nav-link text-white">Titulo 1</a>
+              </li>
+              <li class="nav-item text-center">
+                <a href="" class="nav-link text-white">Titulo 2</a>
+              </li>
+              <li class="nav-item text-center">
+                <a href="" class="nav-link text-white">Titulo 3</a>
+              </li>
+              <li class="nav-item text-center">
+                <a href="" class="nav-link text-white">Titulo 4</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+    `;
+  }
+}
+customElements.define("header-stephen-king", MyHeader);
